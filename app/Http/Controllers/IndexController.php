d04c461ebbebb3779ca09e933a7b75e57ecfd84d
Enflares <info@enflares.com>
2021-12-16 21:54:46 +1100

    Initialize Laravel Blog Project with connection to MySQL database.

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $mysql = DB::connection('mysql');
+        dd($mysql);
+    }
+}
